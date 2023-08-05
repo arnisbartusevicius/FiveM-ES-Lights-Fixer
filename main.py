3e40d4507ceb19fe80bfcce061ab5f5d0177d620
Arnis Bartusevicius <arnisbartusevicius@proton.me>
2023-08-05 10:59:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,57 @@ import re
 import easygui
 import random
 
-def generate_id():
+def generate_id(existing_ids):
     while True:
-        new_id = str(random.randint(1000000, 9999999))
-        if not any(abs(int(new_id[i]) - int(new_id[i+1])) == 1 for i in range(len(new_id)-1)):
+        new_id = str(random.randint(14, 255))
+        if new_id not in existing_ids:
+            existing_ids.add(new_id)
             return new_id
+
+def get_id(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    match = re.search(r'<id value="(\d+)" />', content)
+    if match:
+        return match.group(1)
+    return None
 
 def replace_id(file_path, old_id, new_id):
     with open(file_path, 'r') as file:
         content = file.read()
-    content = content.replace(old_id, new_id)
-    matches = re.findall(r'\b' + old_id + r'\b', content)
-    for match in matches: 
-        content = content.replace(match, new_id)
+    content = content.replace(f'{old_id}', f'{new_id}')
     with open(file_path, 'w') as file:
         file.write(content)
 
-def replace_id_in_folders(folder_paths):
-    for folder_path in folder_paths:
-        file_list = os.listdir(folder_path)
-        file_list = [f for f in file_list if f.endswith('meta')]
-        with open(os.path.join(folder_path, file_list[0]), 'r') as file:
-            content = file.read()
-            old_id = re.search(r'\b\d{6,7}\b', content).group(0)
-        new_id = generate_id()
-        for file_name in file_list:
-            file_path = os.path.join(folder_path, file_name)
-            replace_id(file_path, old_id=old_id, new_id=new_id)
+def replace_duplicate_ids(folder_path):
+    car_folders = []
+
+    for root, dirs, files in os.walk(folder_path):
+        for dir in dirs:
+            car_folder_path = os.path.join(root, dir)
+            carcols_file_path = os.path.join(car_folder_path, 'carcols.meta')
+            carvars_file_path = os.path.join(car_folder_path, 'carvariations.meta')
+            
+            if os.path.exists(carcols_file_path) and os.path.exists(carvars_file_path):
+                car_folders.append(car_folder_path)
+
+    existing_ids = set()
+
+    for car_folder in car_folders:
+        carcols_file_path = os.path.join(car_folder, 'carcols.meta')
+        carvars_file_path = os.path.join(car_folder, 'carvariations.meta')
+
+        old_id = get_id(carcols_file_path)
+        if old_id:
+            if old_id in existing_ids or int(old_id) > 255:
+                new_id = generate_id(existing_ids)
+                replace_id(carcols_file_path, old_id, new_id)
+                replace_id(carvars_file_path, old_id, new_id)
+            else:
+                existing_ids.add(old_id)
 
 dir_path = easygui.diropenbox(title="Select the directory containing your vehicles.")
 if dir_path:
-    folder_paths = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, f))]
-    choices = [os.path.basename(f) for f in folder_paths]
-    selected_folders = easygui.multchoicebox("Choose folders that contain broken vehicles", "Select Broken Vehicles", choices=choices)
-    if selected_folders:
-        selected_paths = [folder_paths[i] for i in range(len(folder_paths)) if choices[i] in selected_folders]
-        replace_id_in_folders(selected_paths)
-        
-easygui.msgbox("The broken lights have been fixed. If they are still broken, use this script again on the files with broken lights.\n-Arnis", "Success")
+    replace_duplicate_ids(dir_path)
+
+easygui.msgbox("The script has successfully fixed all broken ES lights.\n-Arnis", "Success")
